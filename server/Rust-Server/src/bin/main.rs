86dcea135fbed7f78ba33b8b93afa634abf3f585
Max Kelly <maxrkelly@protonmail.com>
2019-04-18 18:16:45 -0400

    adding rust server code

--- server/Rust-Server/src/bin/main.rs
+++ server/Rust-Server/src/bin/main.rs
@@ -1,13 +1,16 @@
-extern crate ftp;
+// extern crate ftp;
 
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 use std::fs;
+use std::str;
+// use std::{error::Error};
+// use signal_hook::{iterator::Signals, SIGINT};
 
-use ftp::FtpStream; 
+// use ftp::FtpStream; 
 
-use Rust_Server::ThreadPool;
+// use Rust_Server::ThreadPool;
 
 // struct ThreadPool;
 // impl ThreadPool {
@@ -19,14 +22,19 @@ use Rust_Server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    // let pool = ThreadPool::new(4);
+    // let signals = Signals::new(&[SIGINT])?;
 
-    for stream in listener.incoming().take(2){
+    for stream in listener.incoming().take(5){
     	let stream = stream.unwrap();
 
-    	pool.execute(|| {
+        print!("incoming IP {:?} ", stream.local_addr().unwrap());
+
+        
+
+    	// pool.execute(|| {
             handle_connection(stream);
-        });
+        // });
     }
 
     println!("shutting down");
@@ -38,10 +46,14 @@ fn handle_connection(mut stream: TcpStream){
 	stream.read(&mut buffer).unwrap();
 
 	let get_home = b"GET / HTTP/1.1\r\n";
+    let simple_home = b"home\n";
 	let get_file = b"GET /file HTTP/1.1\r\n";
+    let simple_file = b"file";
 
 	//  get localhost:port/
-	if buffer.starts_with(get_home) {
+	if buffer.starts_with(get_home) || buffer.starts_with(simple_home) {
+
+        println!("request: {:?}", str::from_utf8(get_home).unwrap());
 
 		// response code and content
         let response = "HTTP/1.1 200 OK\r\n\r\npenis";
@@ -51,9 +63,11 @@ fn handle_connection(mut stream: TcpStream){
         stream.flush().unwrap();
     } 
 
-    else if buffer.starts_with(get_file) {  //  tries to transfer a file
+    else if buffer.starts_with(get_file) || buffer.starts_with(simple_file) {  //  tries to transfer a file
 
-        let ftp_stream = FtpStream::connect("127.0.0.1:21").unwrap();
+        println!("request: {:?}", str::from_utf8(get_file).unwrap());
+
+        // let ftp_stream = FtpStream::connect("127.0.0.1:21").unwrap();
 
     	let contents = fs::read_to_string("test-file.exe").unwrap();
 
@@ -65,6 +79,8 @@ fn handle_connection(mut stream: TcpStream){
         stream.flush().unwrap();
     } else {
         let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\nfail";
+
+        println!("request: {:?}", status_line);
         let response = format!("{}", status_line);
 
         stream.write(response.as_bytes()).unwrap();
---
+++ server/Rust-Server/target/debug/deps/main-79c4bb48cf3ee11c.d
@@ -0,0 +1,5 @@
+/home/Max/Documents/Github/Rust/server/Rust-Server/target/debug/deps/main-79c4bb48cf3ee11c.rmeta: src/bin/main.rs
+
+/home/Max/Documents/Github/Rust/server/Rust-Server/target/debug/deps/main-79c4bb48cf3ee11c.d: src/bin/main.rs
+
+src/bin/main.rs:
